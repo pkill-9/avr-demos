fe807b61486cbd499f5ec8b1f15a317f177613c3
Matt Signorini <matt@signorini.id.au>
2021-05-21 22:19:20 +1000

    Added a module to generate square wave tones.
    Added function definitions.

---
+++ TonePitchFollower/tone.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "tone.h"
+
+/********************************************************************/
+
+    void
+tone_init (channel)
+    uint8_t channel;
+{
+}
+
+/********************************************************************/
+
+    void
+set_frequency (channel, frequency_level)
+    uint8_t channel;
+    uint8_t frequency_level;
+{
+}
+
+/********************************************************************/
+
+    void
+no_tone (channel)
+    uint8_t channel;
+{
+}
+
+/********************************************************************/
+
+/** vim: set ts=4 sw=4 et : */
---
+++ TonePitchFollower/tone.h
@@ -0,0 +1,20 @@
+/**
+ *  tone.h
+ *
+ *  Functions to generate square wave signals of variable frequency on 
+ *  OCR0A/OCR0B pins.
+ */
+
+#ifndef _TONE_H
+#define _TONE_H
+
+#define CHANNEL_A       0x01
+#define CHANNEL_B       0x02
+
+void tone_init (uint8_t channel);
+void set_frequency (uint8_t channel, uint8_t frequency_level);
+void no_tone (uint8_t channel);
+
+#endif
+
+/** vim: set ts=4 sw=4 et : */
