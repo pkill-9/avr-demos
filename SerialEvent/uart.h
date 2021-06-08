74f959a13f067c355a862b9aed25bb94518ac68a
Matt Signorini <matt@signorini.id.au>
2021-06-08 19:32:49 +1000

    Created new demo for SerialEvent.
    The Arduino version is a sketch that reads a line of text from the Serial, and
    then echos the same text back on the serial.
    
    So far, I've added an extra getline function to the uart library.

---
+++ SerialEvent/uart.h
@@ -0,0 +1,22 @@
+/**
+ *  uart.h
+ *
+ *  Functions to transmit and receive data via built in UART hardware
+ */
+
+#ifndef _UART_H
+#define _UART_H
+
+#include <string.h>
+
+void uart_init (unsigned long baud_rate);
+size_t transmit_string (const char *message);
+size_t transmit_int (int value);
+uint8_t tx_slots_free (void);
+
+char uart_getchar (void);
+size_t uart_getline (char *buffer, size_t max_length);
+
+#endif // _UART_H
+
+// vim: ts=4 sw=4 et
