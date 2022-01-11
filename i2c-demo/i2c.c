80110c5bfc93defd617d26e251213bb1e7b477ad
Matt Signorini <matt@signorini.id.au>
2022-01-11 15:42:09 +1100

    Begun work on a simple I2C library.
    This first version will only provide the capability to transmit data from
    the ATmega chip to an external chip using master transmitter mode.
    
    As of this commit, I've added type definitions for a buffer of data to
    transmit (in a linked list) and also basic function prototypes.

---
+++ i2c-demo/i2c.c
@@ -0,0 +1,136 @@
+/**
+ *  i2c.c
+ *
+ *  Functions for interacting with Atmel microcontroller TWI (two wire 
+ *  interface) hardware. Atmel TWI is inter-operable with I2C. These 
+ *  functions enable the calling code to transsfer data to and from other
+ *  devices connected to the microcontroller via an I2C bus.
+ */
+
+#include <avr/io.h>
+
+#include "i2c.h"
+
+/********************************************************************/
+
+#define BUFFER_LENGTH 32
+
+// Each I2C transfer must contain a device address, and the data to send.
+// For most devices, the first byte in the data packet specifies the register
+// number on the device that is to be written to.
+struct i2c_queue_item
+{
+    uint8_t device_address;
+    uint8_t i2c_mode;
+    const uint8_t *data;
+    struct i2c_queue_item *next;
+};
+
+// these constants are used to determine which mode to put the I2C hardware in
+// for the current transmission.
+#define MASTER_TRANSMITTER_MODE 0x02
+#define MASTER_RECEIVER_MODE 0x04
+
+
+static struct i2c_queue_item i2c_buffer [BUFFER_LENGTH];
+
+static struct i2c_queue_item *queue_head;
+static struct i2c_queue_item *queue_tail;
+
+/********************************************************************/
+
+static struct i2c_queue_item *allocate_queue_slot (void);
+static void master_transmitter_handler (void);
+
+/********************************************************************/
+
+/**
+ *  Prepare the ATmega328P TWI hardware for data transfers.
+ */
+    void
+i2c_init (void)
+{
+    queue_head = NULL;
+    queue_tail = NULL;
+
+    // step through all the slots in the buffer and mark them as free by
+    // setting the i2c_mode to zero.
+    for (int i = 0; i < BUFFER_LENGTH; i ++)
+        i2c_buffer [i].i2c_mode = 0x00;
+}
+
+/********************************************************************/
+
+/**
+ *  Send data to a specified destination over the I2C bus.
+ *
+ *  Note that sending is asynchronous; this function will place the data in
+ *  a queue of pending I2C transactions which will be executed when the I2C
+ *  bus has finished any other read/writes.
+ *
+ *  This function does not copy the data to another buffer, so the caller
+ *  must take care not to modify the data in the buffer before sending is
+ *  complete. This includes after this function returns; since the I2C bus
+ *  speed is an order of magnitude or two slower than the CPU clock speed.
+ */
+    void
+i2c_send_to (device_address, data, length)
+    uint8_t device_address;     // 8 bit I2C device address
+    const char *data;           // data to send (one or more bytes)
+    unsigned int length;        // number of bytes to send
+{
+}
+
+/********************************************************************/
+
+/**
+ *  Handle events when the hardware is in master transmitter mode.
+ *
+ *  This function will check the value in the status register (TWSR) and
+ *  take the appropriate action by loading values into the data register
+ *  and/or control register
+ */
+    void
+master_transmitter_handler (void)
+{
+    uint8_t status_code = TWSR & 0xF8;
+
+    switch (status_code)
+    {
+    case 0x08:
+    case 0x10:
+        // START or REPEAT START has been sent; load slave address + write
+        // bit into TWDR.
+        break;
+
+    case 0x18:
+    case 0x20:
+        // slave address + write has been transmitted and ACK received. load
+        // data byte into TWDR.
+        // TODO: 0x20 indicates that NOT ACK was received, should this be
+        // considered an error?
+        break;
+
+    case 0x28:
+    case 0x30:
+        // data has been transmitted and either ACK (0x28) or NOT ACK (0x30)
+        // has been received. Load next data byte into TWDR.
+        break;
+
+    case 0x38:
+        // Arbitration lost. This can only happen if there is another device
+        // trying to become master on the I2C bus, and is not applicable to
+        // this code.
+        //
+        // fall through to default.
+
+    default:
+        // status code not defined in datasheet. This code should never be
+        // reached.
+        break;
+    }
+}
+
+/********************************************************************/
+
+/** vim: set ts=4 sw=4 et : */
--- i2c-demo/i2c.h
+++ i2c-demo/i2c.h
@@ -8,6 +8,8 @@
 #ifndef _I2C_H
 #define _I2C_H
 
+#include <stdint.h>
+
 void i2c_init (void);
 void i2c_send_to (uint8_t device_address, const char *data, unsigned int length);
 uint8_t i2c_read_register (uint8_t device_address, uint8_t device_register);
