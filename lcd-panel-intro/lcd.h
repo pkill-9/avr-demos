27f66948045f5753771f2762f69e7dab67b6fd95
Matt Signorini <matt@signorini.id.au>
2022-12-01 17:51:59 +1100

    Begun working on a demo for an LCD panel.
    This will work with a graphical LCD panel that I bought. To begin with the
    demo will simply cycle through a sequence of colours.
    
    At this stage, the main program is complete; I now need to add the logic to
    operate the LCD panel.

---
+++ lcd-panel-intro/lcd.h
@@ -0,0 +1,40 @@
+/**
+ *  lcd.h
+ *
+ *  Defines functions and constants to interact with a graphical LCD panel.
+ */
+
+#ifndef _LCD_H
+#define _LCD_H
+
+//
+// constants for 16 bit (RGB 565) colours
+//
+#define COLOUR_BLACK            0x0000
+#define COLOUR_NAVY             0x000F
+#define COLOUR_DARK_GREEN       0x03E0
+#define COLOUR_DARK_CYAN        0x03EF
+#define COLOUR_MAROON           0x7800
+#define COLOUR_PURPLE           0x780F
+#define COLOUR_OLIVE            0x7BE0
+#define COLOUR_LIGHT_GREY       0xC618
+#define COLOUR_DARK_GREY        0x7BEF
+#define COLOUR_BLUE             0x001F
+#define COLOUR_GREEN            0x07E0
+#define COLOUR_CYAN             0x07FF
+#define COLOUR_RED              0xF800
+#define COLOUR_MAGENTA          0xF81F
+#define COLOUR_YELLOW           0xFFE0
+#define COLOUR_ORANGE           0xFD20
+#define COLOUR_WHITE            0xFFFF
+#define COLOUR_PINK             0xFE19
+#define COLOUR_SKY_BLUE         0x867D
+
+
+void lcd_init (void);
+void lcd_fill_colour (uint16_t colour);
+
+
+#endif // _LCD_H
+
+/** vim: set ts=4 sw=4 et: */
---
+++ lcd-panel-intro/main.c
@@ -0,0 +1,76 @@
+/**
+ *  LCD PANEL INTRO
+ *
+ *  This is an AVR program that will talk to a colour (RGB) LCD panel over the
+ *  SPI bus. This code is designed to work with an ATmega328P MCU and connect
+ *  to a DFRobot 320x240px display (SKU: DFR0664 for reference) because that
+ *  is what I have to work with. The display panel driver chip is an ST7789V.
+ *
+ *  This program will:
+ *      - Initialise the SPI bus controller on the MCU side.
+ *      - Initialise the LCD panel by sending commands to the ST7789V over the
+ *        SPI bus.
+ *      - Set up a timer on the MCU
+ *      - At every timer alarm, change the screen fill colour (cycle through a
+ *        list of colours).
+ *
+ */
+
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+#include "lcd.h"
+
+/********************************************************************/
+
+//
+// List of colours to cycle through
+//
+const uint16_t colours_list [] = {
+    COLOUR_BLACK, COLOUR_NAVY, COLOUR_DARK_GREEN, COLOUR_DARK_CYAN,
+    COLOUR_MAROON, COLOUR_PURPLE, COLOUR_OLIVE, COLOUR_LIGHT_GREY,
+    COLOUR_DARK_GREY, COLOUR_BLUE, COLOUR_GREEN, COLOUR_CYAN, COLOUR_RED,
+    COLOUR_MAGENTA, COLOUR_YELLOW, COLOUR_ORANGE, COLOUR_WHITE, COLOUR_PINK,
+    COLOUR_SKY_BLUE
+};
+
+#define NUM_COLOURS     19
+
+static volatile int current_colour = 0;
+
+
+/********************************************************************/
+
+    int
+main (void)
+{
+    lcd_init ();
+
+    //
+    // Set timer 1 to generate an interrupt every second
+    //
+    TCCR1B = (TCCR1B & 0xF8) | 0x04;
+    TIMSK1 |= 0x01;
+
+    while (1)
+    {
+        lcd_fill_colour (colours_list [current_colour]);
+
+        sei ();
+        sleep_mode ();
+    }
+
+    return 0;
+}
+
+/********************************************************************/
+
+ISR (TIMER1_OVF_vect)
+{
+    current_colour ++;
+    current_colour %= NUM_COLOURS;
+}
+
+/********************************************************************/
+
+/** vim: set ts=4 sw=4 et : */
