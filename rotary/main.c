a49a1aa5c4b99697fd88bc4a6542c81456466f84
Matt Signorini <matt@signorini.id.au>
2023-07-25 11:48:41 +1000

    Quick demo to work with a rotary encoder.
    Very simple, just print the signal states over the uart. The hardware seems
    to have a bit of an issue with switch bounce inside the rotary encoder itself,
    so I might consider how to resolve that. Either a delay, or I'm wondering
    about using a capacitor between the encoder signal and ground.

---
+++ rotary/main.c
@@ -0,0 +1,91 @@
+/**
+ *  ROTARY ENCODER DEMO
+ *
+ *  How do they work: Internally the rotary encoder has two switches that open
+ *  and close out of phase with each other for each step that the dial turns.
+ *  We can determine the direction it's turning based on which channel changes
+ *  first. Detecting the changing switches is done with the usual pin change
+ *  interrupts.
+ *
+ *  The encoder also has a third switch, if you press down on the dial it also
+ *  has a push button.
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <stddef.h>
+
+#include "uart.h"
+
+
+static volatile int pin_changed = 0;
+
+/********************************************************************/
+
+/**
+ *  Rotary encoder channel A is connected to PD7 and channel B is connected to
+ *  PD6. We will enable the internal pull-up resistors in the M328P for those
+ *  pins, and the rotary encoder will pull them to ground through its common pin.
+ *
+ *  PD6 => PCINT22
+ *  PD7 => PCINT23
+ */
+    int
+main (void)
+{
+    uint8_t pin_states;
+
+    uart_init (9600);
+
+    ///////////////////////////////////
+    // Set port D pins 6 and 7 to output, and enable the pull-ups
+    //
+    DDRD &= ~0xC0;
+    PORTD |= 0xC0;
+
+    ///////////////////////////////////
+    // Enable the two pin change interrupts, PCINT22 and PCINT23.
+    //
+    PCICR |= _BV (PCIE2);
+    PCMSK2 |= (_BV (PCINT22) | _BV (PCINT23));
+
+    for (;;)
+    {
+        if (pin_changed == 0)
+        {
+            sei ();
+            sleep_mode ();
+            continue;
+        }
+
+        pin_changed = 0;
+
+        ///////////////////////////////
+        // Get the states of the rotary encoder pins, and print the details
+        // over the uart.
+        //
+        pin_states = PIND;
+        transmit_string ("Channel A state: ");
+        transmit_int ((pin_states & _BV (PIND7))? 1 : 0);
+        transmit_string ("; \tChannel B state: ");
+        transmit_int ((pin_states & _BV (PIND6))? 1 : 0);
+        transmit_string ("\r\n");
+    }
+
+    return 0;
+}
+
+/********************************************************************/
+
+/**
+ *  Handle the interrupts generated as the rotary encoder is turned.
+ */
+ISR (PCINT2_vect)
+{
+    pin_changed = 1;
+}
+
+/********************************************************************/
+
+/** vim: set ts=4 sw=4 et : */
