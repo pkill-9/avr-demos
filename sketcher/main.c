e894ed5eeae6de6486cb0dbd134fed868b7edfb0
Matt Signorini <matt@signorini.id.au>
2023-07-25 22:19:08 +1000

    Initial work on the sketcher demo.
    This version is going to be simplified to update the coordinates as if we are
    drawing, but then only print the coordinates over the uart.

---
+++ sketcher/main.c
@@ -0,0 +1,116 @@
+/**
+ *  SKETCHER DEMO
+ *
+ *  The hardware consists of a graphical LCD panel, and two rotary encoders.
+ *  Turning the encoders will draw a line on the panel, with each dial
+ *  controlling a set direction (x and y axes).
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+#include <stddef.h>
+
+#include "vectors.h"
+#include "uart.h"
+
+
+static volatile int x_change = 0, y_change = 0;
+
+/********************************************************************/
+
+    int
+main (void)
+{
+    vector_t cursor;
+    uint8_t x_rotary, y_rotary;
+
+    uart_init (9600);
+
+    cursor.x = 120;
+    cursor.y = 160;
+
+    for (;;)
+    {
+        if (x_change == 0 && y_change == 0)
+        {
+            sei ();
+            sleep_mode ();
+            continue;
+        }
+
+        if (x_change)
+        {
+            // delay for debouncing
+            _delay_ms (5);
+            x_change = 0;
+
+            x_rotary = PIND;
+
+            // check if it's turned clockwise or counter clockwise by XORing
+            // the two channels.
+            if ((x_rotary & 0x80) != (x_rotary & 0x40) << 1)
+            {
+                // counter clockwise
+                cursor.x --;
+                cursor.y --;
+            }
+            else
+            {
+                // clockwise
+                cursor.x ++;
+                cursor.y ++;
+            }
+        }
+
+        if (y_change)
+        {
+            _delay_ms (5);
+            y_change = 0;
+
+            y_rotary = PINB;
+
+            // check if it's turned clockwise or counter clockwise by XORing
+            // the two channels.
+            if ((y_rotary & 0x02) != (y_rotary & 0x04) << 1)
+            {
+                // counter clockwise
+                cursor.x ++;
+                cursor.y --;
+            }
+            else
+            {
+                // clockwise
+                cursor.x --;
+                cursor.y ++;
+            }
+        }
+
+        transmit_string ("x: ");
+        transmit_int (cursor.x);
+        transmit_string ("; y: ");
+        transmit_int (cursor.y);
+        transmit_string ("\r\n");
+    }
+
+    return 0;
+}
+
+/********************************************************************/
+
+ISR (PCINT0_vect)
+{
+    x_change = 1;
+}
+
+/********************************************************************/
+
+ISR (PCINT2_vect)
+{
+    y_change = 1;
+}
+
+/********************************************************************/
+
+/** vim: set ts=4 sw=4 et : */
---
+++ sketcher/vectors.c
@@ -0,0 +1,47 @@
+/**
+ *  Functions to work with 2D coordinate pairs.
+ */
+
+#include <stdint.h>
+
+#include "vectors.h"
+
+/********************************************************************/
+
+/**
+ *  Swap the x and y axes of a vector.
+ */
+    void
+swap_axes (v)
+    vector_t *v;
+{
+    uint16_t temp = v->x;
+    v->x = v->y;
+    v->y = temp;
+}
+
+/********************************************************************/
+
+/**
+ *  Swap two vectors. On return, a will contain b's components, and b will
+ *  contain a's components.
+ */
+    void
+swap_vectors (a, b)
+    vector_t *a;
+    vector_t *b;
+{
+    uint16_t temp;
+
+    temp = a->x;
+    a->x = b->x;
+    b->x = temp;
+
+    temp = a->y;
+    a->y = b->y;
+    b->y = temp;
+}
+
+/********************************************************************/
+
+/** vim: set ts=4 sw=4 et : */
